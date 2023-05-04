a7db16a9123d474deb895ad41710f2f506d86cce
zackNarcos <58006990+zackNarcos@users.noreply.github.com>
2023-05-04 01:49:24 +0000

    2 files modified

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -6,12 +6,9 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./customers.component.css'],
 })
 export class CustomerListComponent implements OnInit {
-  @Input() query?: string; // this will come from the query params
   @Input() id?: string; // this will come from the path params
   @Input() title!: string; // this will come from the data
   constructor() {}
 
-  ngOnInit() {
-    alert(this.query);
-  }
+  ngOnInit() {}
 }
